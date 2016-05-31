2530948d87e728f40edbc36064978e37736fbaee
Ningxia Zhang <thmmrs2298@gmail.com>
2016-05-30 17:16:24 -0700

    menu contact styling

--- src/_modules/menu/menu.js
+++ src/_modules/menu/menu.js
@@ -10,6 +10,14 @@ var Menu = function() {
   $('.menu-button').click(function() {
     $(this).toggleClass('active');
     $('.menu-overlay').toggleClass('open');
+
+    // prevent background from scrolling when menu is open
+    if($(this).hasClass('active')) {
+      $('body').addClass('has-menu-open');
+    }
+    else {
+      $('body').removeClass('has-menu-open');
+    }
   });
 };
 
